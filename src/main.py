555e3f907d5b463dfc7cd03f3f1febd57caa211e
Ajaco998 <agjacobson7@gmail.com>
2025-11-08 12:29:54 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,17 +5,33 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
-
+    #poll for events
+    #pygame.QUIT event means that the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill("purple")
 
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
     pygame.display.flip()
 
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
