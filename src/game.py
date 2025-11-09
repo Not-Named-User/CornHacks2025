885791bf29ea2df10d2498203bee9cbb494aa620
Ajaco998 <agjacobson7@gmail.com>
2025-11-08 20:20:00 -0600

    game.py should be up to date

--- src/game.py
+++ src/game.py
@@ -20,41 +20,7 @@ class Player(pygame.sprite.Sprite):    # Child class of Parent class character
         self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
         self.position = list(position)
 
-<<<<<<< HEAD
-    def isAlive(self):
-        if(self.health < 0):
-            self.alive = False  
 
-    def take_damage(self, amount):
-        self.health -= amount
-
-    """
-Player Class for Super Loud
-
-Status: Not finished
-"""
-import game
-class Player(game.Character):    # Child class of Parent class character
-
-    def __init__(self, name, health = 10, level = 1):
-        self.name = name
-        self.health = health
-        self.level = level
-
-    def __repr__(self):
-        return f"{self.name}\nHealth = {self.health}, Level = {self.level}"
-        #print(f"{self.name}: Health = {self.health}, Level = {self.level}")
-    
-    def heal(self, amount):
-        self.health += amount
-        if self.health > 10:
-            self.health = 10
-
-    def level_up(self):
-        self.level += 1
-    
-=======
->>>>>>> PreLaunch
     def printCharacterStats(self):
         print(f"Name: {self.name}, Health: {self.health} Level: {self.level}")
 
